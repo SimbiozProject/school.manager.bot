8f8d63a59ad237e41baaed3f3da61202015b9750
drozdi-k <irisiadrozdi@gmail.com>
2021-04-03 22:10:55 +0300

    Create Bot

---
+++ src/main/java/com/example/demo/WordLandBot/config/Mapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.WordLandBot.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Mapper {
+    @Bean
+    public ObjectMapper objectMapper(){
+        var objectMapper = new ObjectMapper();
+        return objectMapper;
+    }
+}
