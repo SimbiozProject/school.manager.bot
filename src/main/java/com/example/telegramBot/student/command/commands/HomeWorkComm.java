a828d7c34321aaee1feb09e1919b216780d58971
VladimirPetranovski <desantluninec@mail.ru>
2021-05-10 02:17:20 +0300

    changed CommandName.java, UserCommandHandler.java, added method getHomeWork in UserInlineKeyboardSource.java in package user, added CommandName.java, Command/java, HomeWorkComm.java, StudentCommandHandler,java, StudentInlineKeyboardSource.java, StudentReplyKeyboardSource.java, UncnownComm.java in package student

--- src/main/java/com/example/telegramBot/student/command/commands/HomeWorkComm.java
+++ src/main/java/com/example/telegramBot/student/command/commands/HomeWorkComm.java
@@ -2,13 +2,19 @@ package com.example.telegramBot.student.command.commands;
 
 import com.example.telegramBot.service.SendBotMessageService;
 import com.example.telegramBot.student.keyboard.inline.StudentInlineKeyboardSource;
+<<<<<<< HEAD
 import com.example.telegramBot.student.keyboard.reply.StudentReplyKeyboardSource;
+=======
+>>>>>>> changed CommandName.java, UserCommandHandler.java, added method getHomeWork in UserInlineKeyboardSource.java in package user, added CommandName.java, Command/java, HomeWorkComm.java, StudentCommandHandler,java, StudentInlineKeyboardSource.java, StudentReplyKeyboardSource.java, UncnownComm.java in package student
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 
 public class HomeWorkComm implements Command {
 
+<<<<<<< HEAD
     private final StudentReplyKeyboardSource studentReplyKeyboardSource = new StudentReplyKeyboardSource();
+=======
+>>>>>>> changed CommandName.java, UserCommandHandler.java, added method getHomeWork in UserInlineKeyboardSource.java in package user, added CommandName.java, Command/java, HomeWorkComm.java, StudentCommandHandler,java, StudentInlineKeyboardSource.java, StudentReplyKeyboardSource.java, UncnownComm.java in package student
     private final StudentInlineKeyboardSource studentInlineKeyboardSource = new StudentInlineKeyboardSource();
     private final static String HOME_WORK_MESSAGE = "Здусь ты можешь получить домашнее задание и отправить ее на проверку преподавателю";
     private final ReplyKeyboard homeWork = studentInlineKeyboardSource.getHomeWorkKeyboard();
