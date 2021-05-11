9e5cb5911a42604ca41c73069b17ee862b82a02f
VladimirPetranovski <desantluninec@mail.ru>
2021-05-11 23:56:50 +0300

    changed StudentReplyKeyboardSource.java

--- src/main/java/com/example/telegramBot/student/keyboard/reply/StudentReplyKeyboardSource.java
+++ src/main/java/com/example/telegramBot/student/keyboard/reply/StudentReplyKeyboardSource.java
@@ -1,8 +1,6 @@
 package com.example.telegramBot.student.keyboard.reply;
 
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
