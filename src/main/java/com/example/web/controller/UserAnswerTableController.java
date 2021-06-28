5130f3df7295d95b12714678bf94a4d41e24a4d4
avp02 <vpetranovskij@gmail.com>
2021-06-29 00:44:16 +0300

    changed JP-71

--- src/main/java/com/example/web/controller/UserAnswerTableController.java
+++ src/main/java/com/example/web/controller/UserAnswerTableController.java
@@ -4,6 +4,7 @@ import com.example.web.dao.service.TgUserTableDaoWebService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -19,4 +20,17 @@ public class UserAnswerTableController {
         return modelAndView;
     }
 
+    @GetMapping(value = "/searchUserName")
+    public ModelAndView searchByUserName(@ModelAttribute(name = "userName") String userName) {
+        ModelAndView modelAndView = new ModelAndView("/allUsersTest");
+        modelAndView.addObject("listTgUserTable", tgUserTableDaoWebService.findByUserName(userName));
+        return modelAndView;
+    }
+    //seaarchById=12&searchByName=Anton
+    @GetMapping(value = "/searchId")
+    public ModelAndView searchById(@ModelAttribute(name = "id") Long id) {
+        ModelAndView modelAndView = new ModelAndView("/allUsersTest");
+        modelAndView.addObject("listTgUserTable", tgUserTableDaoWebService.findById(id));
+        return modelAndView;
+    }
 }
